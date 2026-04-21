72cc1d076eb4ada00fc30bab47664883d9466a45
anastasaa2006-creator <anastasaa2006@gmail.com>
2026-04-21 17:32:46 +0300

    Добавлен класс Julia для фрактала Жюлиа

---
+++ src/ru/gr0946x/ui/fractals/Julia.java
@@ -0,0 +1,45 @@
+package ru.gr0946x.ui.fractals;
+
+import ru.smak.math.Complex;
+
+public class Julia implements Fractal {
+
+    private final int maxIterations = 500;
+    private final double R2 = 4.0;
+
+    private double cReal;
+    private double cImag;
+
+    public Julia() {
+        this.cReal = -0.8;
+        this.cImag = 0.156;
+    }
+
+
+    public void setC(double real, double imag) {
+        this.cReal = real;
+        this.cImag = imag;
+    }
+
+    @Override
+    public float inSetProbability(double x, double y) {
+        Complex z = new Complex(x, y);
+        Complex c = new Complex(cReal, cImag);
+
+        int i = 0;
+        while (z.getAbsoluteValue2() < R2 && ++i < maxIterations) {
+            z.timesAssign(z);
+            z.plusAssign(c);   
+        }
+
+        return (float) i / maxIterations;
+    }
+
+    public int getMaxIterations() {
+        return maxIterations;
+    }
+
+    public double getR2() {
+        return R2;
+    }
+}
